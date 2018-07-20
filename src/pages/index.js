02d3e451a24096cd5c97e85e96674a2001aeeb3b
rubz019 <ruben.desert@eemi.com>
2018-07-20 09:05:15 +0200

    little modif

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,11 @@ import { Container, Title } from 'bloomer'
 
 const IndexPage = () => (
   <Container hasTextAlign="centered">
-    <Title isSize="1">Gatsby + Bulma & Bloomer</Title>
+    <Title isSize="1">Il ne sert à rien de dire “Nous avons fait de notre mieux”. Il faut réussir à faire ce qui est nécessaire.</Title>
     <Title isSize="3">
-      Easy <strong>vertical centering</strong> in <strong>fullscreen</strong>
+      Winston <strong>Churchill</strong> - Politicien &#x2690;
     </Title>
-    <Title isSize="4">Include any content you want, it's always centered</Title>
+    <Title isSize="4">30/11/1874 - 24/01/1965</Title>
   </Container>
 )
 
